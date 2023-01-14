1a5ec0f06068819fc4775cddcec722be7f09868c
Namratha2604 <119146009+Namratha2604@users.noreply.github.com>
2023-01-14 21:18:35 +0530

    Create tute53.js

---
+++ tute53.js
@@ -0,0 +1,35 @@
+// console.log("this is tute 53")
+
+// function greet(names, greetText){
+//     console.log(names+  " is a good boy")
+//     console.log(greetText+ names3 + " is a good boy")
+// }
+
+// let names="harry";
+// let names1="garry";
+// let names2="marry";
+// let names3="sathvik";
+// let greetText="good morning"
+// greet(names,greetText)
+// greet(names1,greetText)
+// greet(names2,greetText)
+// greet(names3,greetText)
+
+// console.log(names + " is a good boy"); 
+
+function hello(files,filestext="how are you"){
+    console.log(filestext+ " " +files+" this is good")
+}
+function sum(a,b,c){
+    let d= a+b+c
+    return d
+}
+let files="nam's";
+let files1="lucy";
+let files2="jack sparrow";
+let filestext="wassup";
+hello(files,filestext);
+hello(files1,filestext);
+hello(files2);
+let returnVal = sum(1,2,4,5,6)
+console.log(returnVal)
